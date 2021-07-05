5ab5955e060436ca158203e3a3f3cc7ed9a868b9
segunBabs <segunBabs@users.noreply.github.com>
2021-07-05 17:15:44 +0000

    deploy: 8b42dd38cdb833f5dd69a2ee70936695352c13e7

---
+++ precache-manifest.55277bf5daa1217f1d77c1e244183898.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dff4dd37107ce942bef87755e5547e75",
+    "url": "https://segunBabs.github.io/task-management-frontend/index.html"
+  },
+  {
+    "revision": "3004caa6066d82d93007",
+    "url": "https://segunBabs.github.io/task-management-frontend/static/css/main.5ac26d30.chunk.css"
+  },
+  {
+    "revision": "95914956599a74f3ed8a",
+    "url": "https://segunBabs.github.io/task-management-frontend/static/js/2.13e9b1b6.chunk.js"
+  },
+  {
+    "revision": "3004caa6066d82d93007",
+    "url": "https://segunBabs.github.io/task-management-frontend/static/js/main.a172101e.chunk.js"
+  },
+  {
+    "revision": "7d617d3235da1a074867",
+    "url": "https://segunBabs.github.io/task-management-frontend/static/js/runtime~main.f88cac1c.js"
+  }
+]);
